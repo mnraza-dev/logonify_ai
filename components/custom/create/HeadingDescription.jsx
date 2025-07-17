ce9e12c7d1c4ca4950dd5c835c25be5ae0ee4b8c
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-17 21:07:30 +0530

    Update HeadingDescription to display default title; enhance LogoTitle to synchronize title state with search parameters.

--- components/custom/create/HeadingDescription.jsx
+++ components/custom/create/HeadingDescription.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const HeadingDescription = ({ title, description }) => {
   return (
     <div>
-      <h2 className="font-bold text-3xl text-primary">{title}</h2>
+      <h2 className="font-bold text-3xl text-primary">
+        {title.length > 0 ? title : "Logo Title"}
+      </h2>
       <p className="text-lg text-gray-500 mt-1">{description}</p>
     </div>
   );
--- components/custom/create/LogoTitle.jsx
+++ components/custom/create/LogoTitle.jsx
@@ -1,21 +1,25 @@
-"use client"
-import React, { useState } from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import HeadingDescription from "./HeadingDescription";
 import { Input } from "@/components/ui/input";
 import { useSearchParams } from "next/navigation";
 
-const LogoTitle = ({onHandleInputChange}) => {
+const LogoTitle = () => {
   const searchParams = useSearchParams();
-  const [title, setTitle] = useState(searchParams?.get("title"));
+  const [title, setTitle] = useState(searchParams?.get("title") || "");
+
+  useEffect(() => {
+    setTitle(searchParams?.get("title") || "");
+  }, [searchParams]);
+
   return (
     <div className="mt-10">
       <HeadingDescription title={title} description="Logo Title Description" />
-
       <Input
-        defaultValue={title}
-        onChange={(e) => onHandleInputChange(e.target.value)}
-        className={" p-4 border rounded-lg mt-5 w-full"}
-        type={"text"}
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        className="p-4 border rounded-lg mt-5 w-full"
+        type="text"
         placeholder="Enter the Logo Title"
       />
     </div>
